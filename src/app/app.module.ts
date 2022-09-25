0df39907bb598bc06e520a95a43a4875ff3ee853
unknown <jesicaalvarez1993@gmail.com>
2022-09-25 17:34:03 -0300

    creacion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { BarraDeNavegacionComponent } from './componentes/barra-de-navegacion/barra-de-navegacion.component';
+import { SobreMiComponent } from './componentes/sobre-mi/sobre-mi.component';
+import { HabilidadesComponent } from './componentes/habilidades/habilidades.component';
+import { ResumenComponent } from './componentes/resumen/resumen.component';
+import { PortafolioComponent } from './componentes/portafolio/portafolio.component';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    BarraDeNavegacionComponent,
+    SobreMiComponent,
+    HabilidadesComponent,
+    ResumenComponent,
+    PortafolioComponent,
+    ContactoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
