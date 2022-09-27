04bd1c08d14d0a3beb60c2d33e528968f2715326
unknown <jesicaalvarez1993@gmail.com>
2022-09-27 20:12:05 -0300

    primer-servicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
-import { BarraDeNavegacionComponent } from './componentes/barra-de-navegacion/barra-de-navegacion.component';
+import { MenuComponent } from './componentes/menu/menu.component';
 import { SobreMiComponent } from './componentes/sobre-mi/sobre-mi.component';
 import { HabilidadesComponent } from './componentes/habilidades/habilidades.component';
 import { ResumenComponent } from './componentes/resumen/resumen.component';
 import { PortafolioComponent } from './componentes/portafolio/portafolio.component';
 import { ContactoComponent } from './componentes/contacto/contacto.component';
 import { FooterComponent } from './componentes/footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     EncabezadoComponent,
-    BarraDeNavegacionComponent,
     SobreMiComponent,
     HabilidadesComponent,
     ResumenComponent,
     PortafolioComponent,
     ContactoComponent,
-    FooterComponent
+    FooterComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
